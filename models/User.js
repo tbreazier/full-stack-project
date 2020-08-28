b6e019471fd34c6a5ac904de413d2ee1ff091b75
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-08-27 20:47:45 -0500

    initial commit for database and models

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+
+
+module.exports = User;
