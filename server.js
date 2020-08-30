d64f52b2f994eea1405368720d6cf1a067c9f251
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-29 20:25:41 -0500

    attempting reddit api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const exphbs = require('express-handlebars');
 const express = require('express');
 const path = require('path');
-
+const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -37,15 +37,26 @@ const photos = [
     },
 ];
 
+let url = 'https://www.reddit.com/.json?limit=5';
+
+let settings = { method: "Get"};
+
+fetch(url, settings)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+});
+    // data.children.data.title
+    // data.children.data.url_overriden_by_dest
 const trendingposts = [
     {
-        trending_title: 'Trending 1',
+        title: 'Trending 1',
     },
     {
-        trending_title: 'Trending 2',
+        title: 'Trending 2',
     },
     {
-        trending_title: 'Trending 3',
+        title: 'Trending 3',
     },
 ];
 
