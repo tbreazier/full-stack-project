64fe613835f6d96d67e07047318c111954533476
geocode-matt <mlindenx@gmail.com>
2020-08-31 17:57:32 -0500

    fixed some styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 const { User, Post} = require('../../models');
 // const withAuth = require('../../utils/auth');
 
-// // POST /api/posts
-// router.post('/', withAuth, (req, res) => {
-//     Post.create({
-//         title: req.body.title,
-//         content: req.body.content,
-//         user_id: req.session.user_id
-//     })
-//         .then((dbPostData) => res.json(dbPostData))
-//         .catch((err) => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+// POST /api/posts
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // PUT /api/posts/1
 router.put('/:id', (req, res) => {
